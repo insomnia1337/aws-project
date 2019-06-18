8536e18300d6848dfc679594706b72bdd7b32846
mateusz.bieda <mateusz.bieda@comarch.pl>
2019-06-18 10:40:53 +0200

    Updates

---
+++ precache-manifest.7c1da8ea17d99879b8ff4a83c668564f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e81633765f6c6b7d3bd4005a4626eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05004421075d4217522f",
+    "url": "/static/css/main.c7a54eb3.chunk.css"
+  },
+  {
+    "revision": "acf6501dbdadee0f729f",
+    "url": "/static/js/2.184dd9e6.chunk.js"
+  },
+  {
+    "revision": "05004421075d4217522f",
+    "url": "/static/js/main.562bb4b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
